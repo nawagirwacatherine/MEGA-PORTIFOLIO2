9402f6e7915e0d99aa021dbc705074a76dd5bfb9
Nawagirwa catherine <134449186+nawagirwacatherine@users.noreply.github.com>
2024-11-15 05:25:13 +0200

    I removed the code which i had used to test the functionality of my app

--- mega-portifolio/src/App.jsx
+++ mega-portifolio/src/App.jsx
@@ -1,23 +1,14 @@
-import { useState } from 'react';
+
 import './App.css'
 
 function App() {
-  const [number,setNumber] = useState(20);
+  
 
-  const incrementNumber = () => {
-    setNumber(number +20);
-  };
-
-  const decreaseNumber = () => {
-    setNumber(number -1);
-  };
 
   return (
     <>
     <div>
-      <p>{number}</p>
-      <button onClick={incrementNumber}>Increment</button>
-      <button onClick={decreaseNumber}>decrease</button>
+     
     </div>
    
     </>
