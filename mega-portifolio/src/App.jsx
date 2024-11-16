a5853c85d7efeadee6db3bed9a68dce0a5da8954
Nawagirwa catherine <134449186+nawagirwacatherine@users.noreply.github.com>
2024-11-17 01:28:01 +0200

    I had imported react file from react but i have removed since it isn't much required in the updated version of node.js

--- mega-portifolio/src/App.jsx
+++ mega-portifolio/src/App.jsx
@@ -1,21 +1,35 @@
 
-import './styles/Styles.css';
-import Navbar from './components/Navbar';
-import About from './components/About';
-import Projects from './components/Projects';
-import Contact from './components/Contact';
-
-
-function App() {
+import { Routes, Route, Link } from 'react-router-dom';
+import About from './components/About.jsx';
+import Projects from './components/Projects.jsx';
+import Contact from './components/Contact.jsx';
+const App = () => {
   return (
-    <div className="App">
-      <Navbar />
-      <About />
-      <Projects />
-      <Contact />
+    <div>
+    
+      <h1>My Portfolio</h1>
+      <nav className="nav">
+
+<div className="nav-items-wrapper">
+  
+  <Link to="/about" className="nav-item">About</Link>
+  <Link to="/projects" className="nav-item">Projects</Link>
+  <Link to="/contact" className="nav-item">Contact</Link>
+ 
+ 
+</div>
+
+    <Routes>
+      <Route path="/about" element={<About />} />
+      <Route path="/projects" element={<Projects />} />
+      <Route path="/contact" element={<Contact />} />
+     
+    </Routes>
+    </nav>
+
     </div>
   );
-}
+};
 
 export default App;
 
